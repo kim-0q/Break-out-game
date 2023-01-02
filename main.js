d74404f60ec4f81939986b537dc2c6cff2e32415
Young-Kyu Kim <rladudrb555@gmail.com>
2023-01-02 15:33:23 +0900

    Basic JS
    공 이동 (충돌 x)

---
+++ main.js
@@ -0,0 +1,15 @@
+const ball=document.getElementById('ball');
+
+// let ballpos=ball.getBoundingClientRect().top;
+
+// ballmove = setInterval(()=> {
+//   ball.style.left=ball.offsetLeft+4+'px';
+//   ball.style.top=ball.offsetTop-2+'px';
+// }, 40);
+
+
+// clientHeight: 516
+// clientLeft: 0
+// clientTop: 0
+// clientWidth: 930
+
