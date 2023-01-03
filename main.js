153a07d6b2c95b43752444a661746c9f3c961ce0
Young-Kyu Kim <rladudrb555@gmail.com>
2023-01-03 10:53:27 +0900

    Basic CSS & JS
    커서 감추기, 클릭시 시작, 충돌구현(베타), 바 이동

--- main.js
+++ main.js
@@ -1,15 +1,64 @@
 const ball=document.getElementById('ball');
+const back=document.getElementById('background');
+const bar=document.getElementById('bar');
 
-// let ballpos=ball.getBoundingClientRect().top;
+let ballmove1, ballmove2, ballmove3, ballmove4;
 
-// ballmove = setInterval(()=> {
-//   ball.style.left=ball.offsetLeft+4+'px';
-//   ball.style.top=ball.offsetTop-2+'px';
-// }, 40);
+document.addEventListener('mousemove',function(event){
+    bar.style.left=event.screenX+'px';
+})
 
+let ballmove=function(){ballmove1=setInterval(function(){
+  if(ball.offsetLeft<back.clientWidth-ball.offsetWidth)
+  {
+    ball.style.left=ball.offsetLeft+4+'px';
+    ball.style.top=ball.offsetTop-2+'px';
+  }
+  else
+  {
+    clearInterval(ballmove1);
+    if(ball.offsetTop>back.clientTop)
+    {
+      ballmove2=setInterval(function(){
+      if(ball.offsetTop>back.clientTop)
+        {
+        ball.style.left=ball.offsetLeft-4+'px';
+        ball.style.top=ball.offsetTop-2+'px';
+        }
+      else
+      {
+      clearInterval(ballmove2);
+      if(ball.offsetLeft>back.clientLeft)
+      {
+        ballmove3=setInterval(function(){
+          if(ball.offsetLeft>ball.offsetHeight)
+          {
+            ball.style.left=ball.offsetLeft-4+'px';
+            ball.style.top=ball.offsetTop+1+'px';
+          }
+          else
+          {
+            clearInterval(ballmove3);
+            ballmove4=setInterval(function(){
+              if(ball.offsetTop<bar.offsetTop-ball.offsetHeight)
+              {
+                ball.style.left=ball.offsetLeft+4+'px';
+                ball.style.top=ball.offsetTop+2+'px';
+              }
+              else
+              {
+                clearInterval(ballmove4);
+              }
+            },40)
+          }
+        },40)
+      }
+      }
+      },40)
+    }
+  }
+},40)
+}
 
-// clientHeight: 516
-// clientLeft: 0
-// clientTop: 0
-// clientWidth: 930
+document.addEventListener('click',ballmove);
 
